7ba38be506566b443726d5cf81ca590dfc808bbf
Nicolas Arnold <nicolas.arnold@protonmail.com>
2021-03-27 18:15:57 -0600

    add get arbs function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod get_arbs;
+
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+
+    println!("{:#?}", get_arbs::get_arbs().unwrap());
+
+    // let content = reqwest::get("https://api.binance.com/api/v3/avgPrice?symbol=BTCUSDT").unwrap().text();
+    // println!("body = {:?}", content);
 }
